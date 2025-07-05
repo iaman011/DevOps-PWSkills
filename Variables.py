79c9ec1001edf6a4576a806e5c30f1a139bf3116
Aman <iaman.singh011@gmail.com>
2025-07-05 22:30:43 +0530

    tutorial

---
+++ Variables.py
@@ -0,0 +1,16 @@
+import math as mt  #for using sqrt function
+print("hello")
+
+# there is no need of defining data types in python unlike in java and c++
+# int takes 4 bytes and string takes 1byte for each character
+a=10
+b=20
+
+c=a+b
+
+print (f"sum of {a} and {b} is {c}")
+
+print(mt.sqrt(25))
+
+
+
